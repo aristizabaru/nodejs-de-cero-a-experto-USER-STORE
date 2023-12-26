a0f0557c6673ce4c097cd53b9e5bf42ae0af6fad
Andrés Aristizábal <andres.aristizabal@gmail.com>
2023-12-26 11:19:17 -0500

    feat: add database connection to mongo

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
 
-(async()=> {
+(async () => {
   main();
 })();
 
 
-function main() {
+async function main() {
+
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
 
   const server = new Server({
     port: envs.PORT,
